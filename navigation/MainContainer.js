56f1751a5cae69fc0602086a986507c423439a93
Gabrielzg1 <gabrielzg173@gmail.com>
2022-11-04 01:55:47 -0300

    frontend

---
+++ navigation/MainContainer.js
@@ -0,0 +1,58 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+// Screens
+import HomeScreen from './screens/HomeScreen';
+import StudentScreen from './screens/StudentScreen';
+import TeacherScreen from './screens/TeacherScreen';
+
+//Screen names
+const homeName = "Home";
+const detailsName = "Student";
+const settingsName = "Teacher";
+
+const Tab = createBottomTabNavigator();
+
+function MainContainer() {
+    return (
+        <NavigationContainer>
+            <Tab.Navigator
+                initialRouteName={homeName}
+                screenOptions={({ route }) => ({
+                    tabBarIcon: ({ focused, color, size }) => {
+                        let iconName;
+                        let rn = route.name;
+
+                        if (rn === homeName) {
+                            iconName = focused ? 'home' : 'home-outline';
+
+                        } else if (rn === detailsName) {
+                            iconName = focused ? 'barcode' : 'barcode-outline';
+
+                        } else if (rn === settingsName) {
+                            iconName = focused ? 'camera' : 'camera-outline';
+                        }
+
+                        // You can return any component that you like here!
+                        return <Ionicons name={iconName} size={size} color={color} />;
+                    },
+                })}
+                tabBarOptions={{
+                    activeTintColor: 'red',
+                    inactiveTintColor: 'grey',
+                    labelStyle: { paddingBottom: 10, fontSize: 10 },
+                    style: { padding: 10, height: 90 }
+                }}>
+
+                <Tab.Screen name={homeName} component={HomeScreen} />
+                <Tab.Screen name={detailsName} component={StudentScreen} />
+                <Tab.Screen name={settingsName} component={TeacherScreen} />
+
+            </Tab.Navigator>
+        </NavigationContainer>
+    );
+}
+
+export default MainContainer;
---
+++ navigation/screens/StudentScreen.js
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import { useState } from 'react'
+import { View, Text, Image, Button } from 'react-native';
+import api from '../../api/api';
+
+
+export default function StudentScreen({ navigation }) {
+
+    const [qrcode, setQrcode] = useState('')
+    const handleStudents = async () => {
+        try {
+            const response = await api.get('/students')
+            setQrcode(response.data[1].img)
+            console.log(qrcode);
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Button onPress={handleStudents} title="Press Me"></Button>
+            <Image
+                source={{ uri: qrcode }}
+                style={{ height: 400, width: 400, resizeMode: 'contain' }} />
+
+        </View>
+    );
+}
