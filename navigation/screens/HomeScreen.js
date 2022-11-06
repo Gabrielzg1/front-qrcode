df770b095d679945b7f118c607229a8230f260d3
Gabriel Zanandre Guadagnini <gabrielzg173@gmail.com>
2022-11-06 13:32:03 -0300

    Update HomeScreen.js

--- navigation/screens/HomeScreen.js
+++ navigation/screens/HomeScreen.js
@@ -38,68 +38,6 @@ export default function HomeScreen({ navigation }) {
         }}
       />
 
-<<<<<<< Updated upstream
-    return (
-        <View style={{ flex: 1.5, alignItems: 'center', justifyContent: 'center' }}>
-            <Text
-                onPress={() => alert('This is the "Home" screen.')}
-                style={{ fontSize: 20, fontWeight: 'bold' }}>Bem vindo, digite seu {type} </Text>
-            <SwitchSelector
-                style={{ margin: 50 }}
-                options={optionsSelector}
-                initial={0}
-                onPress={
-                    value => {
-                        setType(value);
-                        if (value == 'RA') {
-                            setKeyboard('numeric')
-                            setColor('red')
-                            setOption("Student")
-                        } else {
-                            setKeyboard('email-address')
-                            setColor('green')
-                            setOption("Teacher")
-                        }
-                    }} />
-
-            <TextInput
-                value={text}
-                onChangeText={setText}
-                placeholder={type}
-                keyboardType={keyboard}
-                selectedColor={color}
-                style={{
-                    height: 40,
-                    margin: 10,
-                    width: 200,
-                    borderWidth: 1,
-                    padding: 10,
-                }}
-            />
-
-            <Button
-                title="Enter"
-                color={color}
-                onPress={() => {
-                    api.get(`/students/${parseInt(text)}`)
-                        .then((response) => {
-                            navigation.navigate({ name: option, params: { text: text } })
-                        })
-                        .catch((error) => {
-                            if (error.response) {
-                                console.log(error.response.data);
-                                console.log(error.response.status);
-                                console.log(error.response.headers);
-                            }
-                        });
-
-                }}
-            />
-
-        </View>
-    );
-}
-=======
       <TextInput
         value={text}
         onChangeText={setText}
@@ -137,4 +75,3 @@ export default function HomeScreen({ navigation }) {
     </View>
   );
 }
->>>>>>> Stashed changes
