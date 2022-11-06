af5597546d9fcc22df3623617e6c86d1e2507d94
Gabrielzg1 <gabrielzg173@gmail.com>
2022-11-05 21:35:21 -0300

    Home Screen

--- navigation/screens/HomeScreen.js
+++ navigation/screens/HomeScreen.js
@@ -1,14 +1,59 @@
-import * as React from 'react';
-import { View, Text, Button } from 'react-native';
+import { useState } from "react";
+import { View, Text, TextInput, Button } from 'react-native';
+import SwitchSelector from 'react-native-switch-selector';
 
 export default function HomeScreen({ navigation }) {
+    const [type, setType] = useState('');
+    const [number, onChangeNumber] = useState(null);
+
+    const optionsSelector = [
+        { label: 'Aluno', value: 'RA', activeColor: "red" },
+        { label: 'Professor', value: 'EMAIL' }
+
+    ];
+    const [keyboard, setKeyboard] = useState('default');
+    const [color, setColor] = useState('red')
+
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <View style={{ flex: 1.5, alignItems: 'center', justifyContent: 'center' }}>
             <Text
                 onPress={() => alert('This is the "Home" screen.')}
-                style={{ fontSize: 26, fontWeight: 'bold' }}>Home Screen</Text>
+                style={{ fontSize: 20, fontWeight: 'bold' }}>Bem vindo, digite seu {type} </Text>
+            <SwitchSelector
+                style={{ margin: 50 }}
+                options={optionsSelector}
+                initial={0}
+                onPress={
+                    value => {
+                        setType(value);
+                        if (value == 'RA') {
+                            setKeyboard('numeric')
+                            setColor('red')
+                        } else {
+                            setKeyboard('email-address')
+                            setColor('green')
+                        }
+                    }} />
 
-
+            <TextInput
+                onChangeText={onChangeNumber}
+                value={number}
+                placeholder={type}
+                keyboardType={keyboard}
+                selectedColor={color}
+                style={{
+                    height: 40,
+                    margin: 10,
+                    width: 200,
+                    borderWidth: 1,
+                    padding: 10,
+                }}
+            />
+            <Button
+                title="Learn More"
+                color={color}
+                accessibilityLabel="Learn more about this purple button"
+            />
 
         </View>
     );
--- navigation/screens/StudentScreen.js
+++ navigation/screens/StudentScreen.js
@@ -1,27 +1,32 @@
 import * as React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { View, Text, Image, Button } from "react-native";
 import api from "../../api/api";
 
 export default function StudentScreen({ navigation }) {
   const [qrcode, setQrcode] = useState("");
+  const ra = 21002974
+
   const handleStudents = async () => {
     try {
-      const response = await api.get("/students");
-      setQrcode(response.data[1].img);
-      console.log(qrcode);
+      const response = await api.get(`/students/${ra}`);
+      setQrcode(response.data.img);
     } catch (error) {
       console.log(error);
     }
   };
+  useEffect(() => {
+    (async () => await handleStudents())();
+  }, [])
+
 
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Button onPress={handleStudents} title="Press Me"></Button>
       <Image
         source={{ uri: qrcode }}
         style={{ height: 400, width: 400, resizeMode: "contain" }}
       />
     </View>
+
   );
 }
