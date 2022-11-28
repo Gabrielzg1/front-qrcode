a37a33ca05f97c141aa165a4994c93d67cf916d4
Gabriel Zanandre Guadagnini <gabrielzg173@gmail.com>
2022-11-27 22:28:41 -0300

    scanner

--- navigation/screens/Teacher/AttendanceScreen.js
+++ navigation/screens/Teacher/AttendanceScreen.js
@@ -1,15 +1,61 @@
-import * as React from "react";
-import { View, Text, Button } from "react-native";
+import { StyleSheet, Text, View, Button } from "react-native";
+import React, { useEffect } from "react";
+import { BarCodeScanner } from "expo-barcode-scanner";
 
 export default function AttendanceScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text style={{ fontSize: 26, fontWeight: "bold" }}>
-        Attendance Screen
-      </Text>
+  const [hasPermission, setHasPermission] = React.useState(false);
+  const [scanData, setScanData] = React.useState();
+  var attendance = "";
 
-      <Button onPress={() => navigation.navigate("Finish")} title="Finish" />
-      <Button onPress={() => navigation.navigate("Teacher")} title="Back" />
+  useEffect(() => {
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  }, []);
+
+  if (!hasPermission) {
+    return (
+      <View style={styles.container}>
+        <Text>Please grant camera permissions to app.</Text>
+      </View>
+    );
+  }
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanData(data);
+    attendance = data;
+    console.log(attendance);
+    console.log(`Data: ${data}`);
+  };
+
+  return (
+    <View style={styles.container}>
+      <BarCodeScanner
+        style={StyleSheet.absoluteFillObject}
+        onBarCodeScanned={scanData ? undefined : handleBarCodeScanned}
+      />
+      {scanData && (
+        <Button title="Confirmar" onPress={() => setScanData(undefined)} />
+      )}
+      <Button
+        title="Finalizar Chamada"
+        onPress={() =>
+          navigation.navigate({
+            name: "Finish",
+            params: { attendance: attendance },
+          })
+        }
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 0.7,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
